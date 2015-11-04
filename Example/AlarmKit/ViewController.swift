9b68871b0d50eb62cfda1b631ca9ca5649043aea
Daniel Brim <brimizer@gmail.com>
2015-11-03 23:27:00 -0800

    converted initializers to be public

--- Example/AlarmKit/ViewController.swift
+++ Example/AlarmKit/ViewController.swift
@@ -11,14 +11,30 @@ import AlarmKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    let alarm = Alarm(hour:Int, minute:Int, {
+        
+    })
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.datePicker.addTarget(self, action: "timeChanged", forControlEvents: UIControlEvents.ValueChanged)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func timeChanged() {
+        let hour:Int, minute = components(self.datePicker.date)
+    }
+    
+    func components(date:NSDate) -> (Int, Int) {
+        let flags = NSCalendarUnit.Hour.union(NSCalendarUnit.Minute)
+        let comps = NSCalendar.currentCalendar().components(flags, fromDate: date)
+        return (comps.hour, comps.minute)
     }
 }
 
